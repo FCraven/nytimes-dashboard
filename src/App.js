97ba2dc28d56221d1a81118f6a5b133d974985fc
FCraven <craven.francis@gmail.com>
2019-03-01 20:26:07 -0500

    bootstrap styiling takes to mapping over articles and creating presentational cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import APITest from './components/APITest'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
--- src/components/APITest.js
+++ src/components/APITest.js
@@ -15,18 +15,31 @@ export default class APITest extends Component {
   async componentDidMount(){
     try {
       const res = await axios.get(`https://api.nytimes.com/svc/topstories/v2/food.json?api-key=${NYTIMES_API_KEY}`)
-      console.log(`apiKey ==>`,res)
+      console.log(`apiKey ==>`,res.data.results)
+      this.setState({
+        articles: res.data.results
+      })
     } catch (error) {
       console.log(error)
     }
-
   }
 
   render() {
+    console.log(`STATE ==>`,this.state)
     return (
-      <div>
-        {this.state.articles}
-      </div>
-    )
+                <div>
+                      {this.state.articles.map(
+                        (article) =>
+                          <div class="card" style={{width: "18rem"}}>
+                                 <img src={article.multimedia[3].url} class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">{article.title}</h5>
+                                    <p class="card-text">{article.abstract}.</p>
+                                    <a href={article.short_url} class="btn btn-primary">Go to the Article</a>
+                                </div>
+                        </div>
+                      )}
+              </div>
+      )
   }
 }
