b3ab0c95025242dad3f805539f96d0496ac2e3cd
FCraven <craven.francis@gmail.com>
2019-04-20 20:09:41 -0400

    revisiting this app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import APITest from './components/APITest'
+import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <APITest />
+        <Navbar />
+        <APITest />
       </div>
     );
   }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './Navbar.css'
+
+export default class  extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      Navbar
+      </div>
+    )
+  }
+}
+
