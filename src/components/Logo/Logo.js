e89570c1054dfe1d046e83169413f787e5aba8bc
FCraven <craven.francis@gmail.com>
2019-04-25 00:09:03 -0400

    Create Headline component in the morning

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,9 +4,12 @@ import './Logo.css'
 const Logo =()=> {
     return (
       <div className='logo'>
+
+       
         <img  className='times-logo'
               src='https://upload.wikimedia.org/wikipedia/commons/7/77/The_New_York_Times_logo.png'
               alt='The New York Times Logo' />
+
       </div>
     )
 }
