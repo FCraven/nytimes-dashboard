4f2bcd62b8fb87228b0e8ddca4f06d73e2417cc2
FCraven <craven.francis@gmail.com>
2019-07-01 15:39:00 -0400

    imported provider and store in index js. wrapped app with Provider and handed it the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-// import { createStore } from 'redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk'
 // import rootReducer from './reducer'; <-- import individual reducers
 // combine reducers then feed them to the createStore
 
 // export default createStore(rootReducer);
+export default {}
