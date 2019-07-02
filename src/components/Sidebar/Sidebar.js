eecd6f82510765226df4a4ccb3abd75b7038ef8a
FCraven <craven.francis@gmail.com>
2019-07-02 12:27:15 -0400

    basic positioning of sidebar attained

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,17 +5,16 @@ const Sidebar =()=> {
   const links = [{name:'World', href:'#world'}, {name:'U.S.', href:'#us'}, {name:'Politics', href:'#politics'}, {name:'N.Y.', href:'#ny'}, {name:'Business', href:'#business'},{name:'Opinion', href:'#opinion'}, {name:'Tech', href:'#tech'}, {name:'Science', href:'#science'}, {name:'Health', href:'#health'}, {name:'Sports', href:'#sports'}, {name:'Arts', href:'#arts'}, {name:'Books', href:'#books'},{name:'Style', href:'#style'},{name:'Food', href:'#food'},{name:'Travel', href:'#travel'},{name:'Magazine', href:'#magazine'},{name:'T Magazine', href:'#t-magazine'}, {name:'Real Estate', href:'#real-estate'}, {name:'Video', href:'#video'}]
 
   return (
-    <aside id="sidebar-container">
-    
-      <nav>
-        <ul className='sidebar'>
-          {links.map((link,idx) =>
-            <a key={idx} href={link.href}>
-              <li key={idx} className='sidebar-link'>{link.name}</li>
-            </a>)}
-        </ul>
-      </nav>
-    </aside>
+
+      <div id="sidebar-container">
+          <ul className='sidebar'>
+            {links.map((link,idx) =>
+              <a key={idx} href={link.href}>
+                <li key={idx} className='sidebar-link'>{link.name}</li>
+              </a>)}
+          </ul>
+      </div>
+
   )
 }
 
